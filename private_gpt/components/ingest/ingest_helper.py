ae8bf4e16ebcad92b44defd9e9aa6597084d5d11
timur <timurka.zavyalov@mail.ru>
2023-12-29 10:02:38 +0300

    git init

--- private_gpt/components/ingest/ingest_helper.py
+++ private_gpt/components/ingest/ingest_helper.py
@@ -1,3 +1,4 @@
+import re
 import logging
 from pathlib import Path
 
@@ -30,6 +31,8 @@ class IngestionHelper:
         documents = IngestionHelper._load_file_to_documents(file_name, file_data)
         for document in documents:
             document.metadata["file_name"] = file_name
+            document.metadata["file_path"] = file_data
+            document.text = re.sub(r'(\s{2,}|\n{2,})', lambda match: match.group()[0]*2, document.text)
         IngestionHelper._exclude_metadata(documents)
         return documents
 
