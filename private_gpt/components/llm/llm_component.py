0219b70d27cf9a714bae8bb7f15d573fc6dd1463
timur <timurka.zavyalov@mail.ru>
2024-01-12 14:35:45 +0300

    git init

--- private_gpt/components/llm/llm_component.py
+++ private_gpt/components/llm/llm_component.py
@@ -34,7 +34,7 @@ class LLMComponent:
                     context_window=settings.llm.context_window,
                     generate_kwargs={},
                     # All to GPU
-                    model_kwargs={"n_gpu_layers": -1},
+                    model_kwargs={"n_gpu_layers": 28},
                     # transform inputs into Llama2 format
                     messages_to_prompt=prompt_style.messages_to_prompt,
                     completion_to_prompt=prompt_style.completion_to_prompt,
