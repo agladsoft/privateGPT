6ae9cb71b8fa29acc3b65211caad84a5c7aba7a2
timur <timurka.zavyalov@mail.ru>
2024-03-04 12:34:27 +0300

    fix: added unload embedding

--- private_gpt/components/ingest/ingest_component.py
+++ private_gpt/components/ingest/ingest_component.py
@@ -63,9 +63,9 @@ class BaseIngestComponentWithIndexLangchain(BaseIngestComponentLangchain, abc.AB
             threading.Lock()
         )  # Thread lock! Not Multiprocessing lock
         self.collection = "all-documents"
-        self._index = self._initialize_index(self.embedding_component.embedding_model)
+        self._index: Chroma = self._initialize_index(self.embedding_component.embedding_model)
 
-    def _initialize_index(self, embedding) -> BaseIndex[IndexDict]:
+    def _initialize_index(self, embedding) -> Chroma:
         """Initialize the index from the storage context."""
         # Load the index with store_nodes_override=True to be able to delete them
         client = chromadb.PersistentClient(path=str(local_data_path))
@@ -135,12 +135,9 @@ class SimpleIngestComponentLangchain(BaseIngestComponentWithIndexLangchain):
     def _save_docs(self, documents: list[Document], ids: List[str]) -> list[Document]:
         logger.info("Transforming count=%s documents into nodes", len(documents))
         time.sleep(15)
-        self._index.from_documents(
+        self._index.add_documents(
             documents=documents,
-            embedding=self.embedding_component,
-            ids=ids,
-            persist_directory=str(local_data_path),
-            collection_name=self.collection,
+            ids=ids
         )
         logger.info("Persisting the index and nodes")
         time.sleep(15)
