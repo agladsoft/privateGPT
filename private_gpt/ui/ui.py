f745f81ca8da75290302b8b8734696fb59e318a9
timur <timurka.zavyalov@mail.ru>
2024-03-04 14:14:54 +0300

    fix: added unload embedding

--- private_gpt/ui/ui.py
+++ private_gpt/ui/ui.py
@@ -245,7 +245,7 @@ class PrivateGptUi:
                 )
 
         return Llama(
-            n_gpu_layers=43,
+            n_gpu_layers=35,
             model_path=path,
             n_ctx=settings().llm.context_window,
             n_parts=1
