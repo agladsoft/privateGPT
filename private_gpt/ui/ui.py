8f335efcf2c753e22f930fdfcebadf5e34250e69
timur <timurka.zavyalov@mail.ru>
2024-03-04 14:25:38 +0300

    fix: added unload embedding

--- private_gpt/ui/ui.py
+++ private_gpt/ui/ui.py
@@ -273,13 +273,13 @@ class PrivateGptUi:
         """
         if is_load_model:
             logger.info("Loaded files")
-            time.sleep(15)
+            time.sleep(5)
             del self._chat_service.index
             del self._ingest_service.ingest_component.embedding_component
             gc.collect()
             torch.cuda.empty_cache()
             logger.info("Cleared db and embeddings")
-            time.sleep(15)
+            time.sleep(5)
             self._ingest_service.ingest_component.embedding_component = self.init_embedding()
             self._chat_service.index = self.init_db()
             self._chat_service.llm = self.init_model()
