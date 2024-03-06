8d04f121e9c92fadc2122eef6a12ec667f6d4030
timur <timurka.zavyalov@mail.ru>
2024-03-06 10:26:57 +0300

    fix: added unload embedding (removed file upload_file.py)

--- private_gpt/ui/ui.py
+++ private_gpt/ui/ui.py
@@ -274,8 +274,6 @@ class PrivateGptUi:
         if is_load_model:
             logger.info("Loaded files")
             gr.Info("Сервер будет перезагружаться, обновите страницу")
-            self._ingest_service.ingest_component.embedding_component = self.init_embedding()
-            self._chat_service.index = self.init_db()
             time.sleep(5)
             sys.exit(1)
         else:
