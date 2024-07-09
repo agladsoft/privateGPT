9f0533259106495f96abe15452358f620dd69add
timur <timurzavalov@gmail.com>
2024-07-09 16:34:27 +0300

    add: added logging from api upload file

--- private_gpt/server/upload/files_router.py
+++ private_gpt/server/upload/files_router.py
@@ -2,7 +2,7 @@ import logging
 from typing import List
 from private_gpt.constants import FILES_DIR
 from fastapi import APIRouter, Request, UploadFile, HTTPException, File
-from private_gpt.server.ingest.ingest_service import IngestService
+from private_gpt.server.ingest.ingest_service import IngestService, logger
 
 # Not authentication or authorization required to get the health status.
 files_router = APIRouter()
@@ -12,6 +12,7 @@ files_router = APIRouter()
 def ingest(request: Request, files: List[UploadFile] = File(...)):
     logging.info(f"Files {files}")
     service = request.state.injector.get(IngestService)
+    logger.info(list(request._form.items()))
     list_files = []
     for file in files:
         if file.filename is None:
