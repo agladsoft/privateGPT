a9276be96429c80dcc81bea32f0a8210a10cfead
timur <timurzavalov@gmail.com>
2024-07-09 16:47:28 +0300

    Revert "fix: revert to prod"
    This reverts commit 29d48993b6c6156dcf5c95a5d1b43de4cdec0110.

--- private_gpt/launcher.py
+++ private_gpt/launcher.py
@@ -11,6 +11,7 @@ from private_gpt.server.completions.completions_router import completions_router
 from private_gpt.server.embeddings.embeddings_router import embeddings_router
 from private_gpt.server.health.health_router import health_router
 from private_gpt.server.ingest.ingest_router import ingest_router
+from private_gpt.server.upload.files_router import files_router
 from private_gpt.settings.settings import Settings
 
 logger = logging.getLogger(__name__)
@@ -24,12 +25,13 @@ def create_app(root_injector: Injector) -> FastAPI:
 
     app = FastAPI(dependencies=[Depends(bind_injector_to_request)])
 
-    app.include_router(completions_router)
-    app.include_router(chat_router)
-    app.include_router(chunks_router)
-    app.include_router(ingest_router)
-    app.include_router(embeddings_router)
+    # app.include_router(completions_router)
+    # app.include_router(chat_router)
+    # app.include_router(chunks_router)
+    # app.include_router(ingest_router)
+    # app.include_router(embeddings_router)
     app.include_router(health_router)
+    app.include_router(files_router)
 
     settings = root_injector.get(Settings)
     if settings.server.cors.enabled:
---
+++ private_gpt/server/upload/files_router.py
@@ -0,0 +1,34 @@
+import logging
+from typing import List
+from private_gpt.constants import FILES_DIR
+from fastapi import APIRouter, Request, UploadFile, HTTPException, File
+from private_gpt.server.ingest.ingest_service import IngestService
+
+# Not authentication or authorization required to get the health status.
+files_router = APIRouter()
+
+
+@files_router.post("/upload", tags=["Files"])
+def ingest(request: Request, files: List[UploadFile] = File(...)):
+    logging.info(f"Files {files}")
+    service = request.state.injector.get(IngestService)
+    list_files = []
+    for file in files:
+        if file.filename is None:
+            raise HTTPException(400, "No file name provided")
+
+        file_location = f"{FILES_DIR}/{file.filename}"
+        with open(file_location, "wb+") as file_object:
+            file_object.write(file.file.read())
+        status = "success"
+        # try:
+        #     message = service.ingest(file_location)
+        # except Exception as ex:
+        #     logging.error(f"Exception is {ex}")
+        #     message, status = "The file was not uploaded", "fail"
+        list_files.append({
+            "file": file.filename,
+            # "message": message,
+            "status": status
+        })
+    return list_files
